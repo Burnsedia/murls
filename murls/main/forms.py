c8b435c2373d079eb2cd11f0d35e6c8d0343a976
arczi0 <arczyy@outlook.com>
2022-08-07 17:54:16 +0200

    Initial commit

---
+++ murls/main/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    username = forms.CharField(
+        label='Login',
+        max_length=20,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.CharField(
+        label='Adres e-mail',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+
+    password1 = forms.CharField(
+        label='Hasło',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+    password2 = forms.CharField(
+        label='Potwórz hasło',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+        # labels = {
+        #     "username": "Twój login",
+        #     "email": "Email",
+        #     "password1": "Haslo",
+        #     "password2": "Potwierdz haslo"
+        # }
+#
+# def LoginForm(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm()
+#     else:
+#         form = AuthenticationForm()
+#
+#     return render(request,'registration/login.html', {'form': form})
---
+++ murls/murls/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for murls project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'murls.settings')
+
+application = get_wsgi_application()
