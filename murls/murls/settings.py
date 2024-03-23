bb76e5902712ea57ae26752c4db49d092a9a1470
Artur <arczyy@outlook.com>
2024-03-23 16:42:31 +0100

    Move secret key to virtual environment variables

--- murls/murls/settings.py
+++ murls/murls/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=29zqe6ow+=qf^5&v7qbjh6r6q)w+*tmh6=0-fpt$qv!sr%u!3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
